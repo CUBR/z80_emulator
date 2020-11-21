86372737c21749de9a1f227b68d28e19b1fbded2
CUBE <cubingminer8@gmail.com>
2020-11-21 00:40:44 +0000

    updated z80.cpp

--- z80_emulator/z80.cpp
+++ z80_emulator/z80.cpp
@@ -6,15 +6,49 @@ void z80::Init()
 
 void z80::Clock()
 {
+	switch (cycle)
+	{
+	default:
+		break;
 
+	case 1:
+		AddressBus = PC;
+		MREQ = true;
+		RD = true;
+		break;
+	case 2:
+
+	}
 }
 
 void z80::Reset()
 {
 }
 
-void z80::ReadMemory(uint16_t Address)
+int z80::ReadMemory(uint16_t Address)
 {
-	
+	MREQ = true;
+	RD = true;
+	WR = false;
+	BUSACK = true;
 
+	AddressBus = Address;
+
+	return 0;
+}
+
+int z80::WriteMemory(uint16_t Address, uint8_t Data)
+{
+	MREQ = true;
+	WR = true;
+	RD = false;
+	BUSACK = false;
+
+	AddressBus = Address;
+	DataBus = Data;
+	return 0;
+}
+
+void OpcodeFetch() {
+	
 }
